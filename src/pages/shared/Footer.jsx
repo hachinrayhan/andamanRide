499236ed584734bfc93df275f145793d9de99313
hachinrayhan <rayhan.hachin@gmail.com>
2023-05-16 22:37:22 +0600

    footer added

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,7 +1,52 @@
 const Footer = () => {
     return (
-        <div>
-            <h1>this is footer</h1>
+        <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-4 bg-dark text-light p-5">
+            <div className="col-12 col-md-6 col-lg-2">
+                <h3>Cruises</h3>
+                <hr />
+                <ul>
+                    <li>Green Ocean 1</li>
+                    <li>ITT Majestic</li>
+                    <li>Nautika Lite</li>
+                    <li>Green Ocean 2</li>
+                    <li>Nautika</li>
+                    <li>Makruzz</li>
+                </ul>
+            </div>
+            <div className="col-12 col-md-6 col-lg-2">
+                <h3>Cab Services</h3>
+                <hr />
+                <ul>
+                    <li>Cabs in Port Blair</li>
+                    <li>Cabs in Havelock Island</li>
+                    <li>Cabs in Neil Island</li>
+                </ul>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4">
+                <h3>Activities</h3>
+                <hr />
+                <ul>
+                    <li>Parasailing at Corbyns Cove Beach</li>
+                    <li>Sea Walking at Havelock Elephant Beach</li>
+                    <li>Cellular Jail</li>
+                    <li>Boat Scuba Diving at Neil Island</li>
+                    <li>Photography in Andaman</li>
+                    <li>Parasailing in Port Blair</li>
+                    <li>Shore Scuba Diving at Havelock</li>
+                    <li>Reef Looker Semi-Submarine Glass Boat Ride at Elephant Beach</li>
+                </ul>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4">
+                <h3>Contact with Us</h3>
+                <hr />
+                <p>9933263867, 9531955441</p>
+                <p>WhatsApp: 9933263867, 9531955441</p>
+                <p>contact@andamancab.in</p>
+                <hr />
+                <p>Shop No. 01, Pulikeezu Bhavan Bargat Line, near juvenile home, DRDO TRANSIST, Nayagaon, Port Blair, Andaman and Nicobar Islands 744106</p>
+                <p>FOLLOW US</p>
+            </div>
+
         </div>
     );
 };
