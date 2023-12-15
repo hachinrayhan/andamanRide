4cfdb7e5a08f40de9989d5ef3a554932f30e1a2a
M Vinod <mvvinod777@gmail.com>
2023-12-15 21:34:43 +0530

    Update ContactUs.jsx
    Contact Info Updated

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -14,26 +14,24 @@ const ContactUs = () => {
                         <div className="col text-center h-100">
                             <CiLocationOn style={{ fontSize: '4rem' }} className="mb-3" />
                             <h5 className="text-warning">Address</h5>
-                            <p className="card-text">Shop No. 01, Pulikeezu Bhavan Bargat Line, near juvenile home, DRDO TRANSIST, Nayagaon, Port Blair, Andaman and Nicobar Islands 744106</p>
+                            <p className="card-text">State Highway 5, Govind Nagar, Andaman and Nicobar Islands 744211</p>
                         </div>
                         <div className="col text-center h-100">
                             <FaEnvelopeOpenText style={{ fontSize: '3rem' }} className="mb-3" />
                             <h5 className="text-warning">Email Address</h5>
-                            <p className="card-text">contact@andamancab.in <br />
-                                Monday to Friday 9am - 7pm</p>
+                            <p className="card-text">andamanrides@gmail.com</p>
                         </div>
                         <div className="col text-center h-100">
                             <GoDeviceMobile style={{ fontSize: '3rem' }} className="mb-3" />
                             <h5 className="text-warning">Contact Info</h5>
-                            <p className="card-text">9933263867, 9531955441</p>
+                            <p className="card-text">+91-9933253906</p>
                         </div>
                     </div>
                 </div>
                 <div style={{ backgroundImage: `url(${'https://www.andamancab.in/assets/img/bgs/bg-about-us.png'})` }} className="m-5 text-light">
                     <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)', padding: '70px 48px' }}>
-                        <h6>KNOW ABOUT US</h6>
-                        <h1>Your Perfect <br /> Ride Partner</h1>
-                        <h5>Andaman Cab Services is a online cab booking agency based in Port Blair offering online car rental services, which has enabled clients to avail our car hire services which has made us outperform th...</h5>
+                        <h6>ABOUT US</h6>
+                        <h1>Your Perfect <br /> Cab Partner</h1>
                     </div>
                 </div>
             </div>
